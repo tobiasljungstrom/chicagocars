d29ffcac3461bca405e7e50db162ea7f7235ace0
Tobias <tobias.ljungstrom@gmail.com>
2016-04-07 14:27:50 +0200

    Add displying of city stats

--- web/scripts/stats.js
+++ web/scripts/stats.js
@@ -15,7 +15,62 @@ function loadStats() {
 function displayStats() {
     console.log("Displaying Stats");
 
+    var allCities = createCitiesArray();
+    var sortedCities = sortAndGroup(allCities);
+
+    displayCityStats(sortedCities);
+
     $("#totalCustomers").html(allCustomers.length);
     $("#totalCars").html(allCars.length);
     $("#totalAppointments").html(allAppointments.length);
 }
+
+function displayCityStats(sortedCities) {
+
+    for (var i = 0; i < sortedCities[0].length; i++) {
+        $("#cityStatsTable").find("tbody").append("<tr><td>" + sortedCities[0][i] +
+            "</td><td>" + sortedCities[1][i] +
+            "</td></tr>")
+    }
+}
+
+function createCitiesArray() {
+    var returnArray = [];
+
+    for (var i = 0; i < allCustomers.length; i++) {
+        var city = extractAddress(allCustomers[i].address);
+        returnArray.push(city[2]);
+    }
+
+    return returnArray;
+}
+
+function sortAndGroup(stringArray) {
+
+    var names = [];
+    var count = [];
+
+    stringArray.sort();
+
+    for (var i = 0; i < stringArray.length; i++) {
+        if (stringArray[i] !== stringArray[i - 1]) {
+            names.push(stringArray[i]);
+            count.push(1);
+        } else {
+            count[count.length - 1]++;
+        }
+    }
+
+    return [names, count];
+}
+
+/*
+function existsInArray(array, value) {
+    for (var i = 0; i < array.length; i++) {
+        if( array[i].match(value)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}*/
